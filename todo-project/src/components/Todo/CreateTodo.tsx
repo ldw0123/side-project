7657ce5412f8025a2e1ea9c18be5c2080f53a829
ldw0123 <ldw0123@gmail.com>
2024-07-15 17:06:36 +0900

    ♻️ 리팩토링

--- todo-project/src/components/Todo/CreateTodo.tsx
+++ todo-project/src/components/Todo/CreateTodo.tsx
@@ -10,20 +10,18 @@ export default function CreateTodo({
   setNewTodo,
 }: CreateTodoProps) {
   return (
-    <>
-      {/* input & submit */}
-      <form onSubmit={writeTodo} className="absolute bottom-1.5">
-        <input
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          className="w-[430px] h-8 ml-2 mr-2 mb-1 pl-2 bg-white rounded-xl"
-          placeholder="write your plan here!"
-        />
-        <button type="submit" className="w-[45px] h-8 bg-white rounded-xl">
-          작성
-        </button>
-      </form>
-    </>
+    // input & submit
+    <form onSubmit={writeTodo} className="absolute bottom-1.5">
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        className="w-[430px] h-8 ml-2 mr-2 mb-1 pl-2 bg-white rounded-xl"
+        placeholder="write your plan here!"
+      />
+      <button type="submit" className="w-[45px] h-8 bg-white rounded-xl">
+        작성
+      </button>
+    </form>
   );
 }
--- todo-project/src/components/Todo/TodoList.tsx
+++ todo-project/src/components/Todo/TodoList.tsx
@@ -52,79 +52,77 @@ export default function TodoList({
   return (
     <ul className="mt-7 ml-4">
       {initialTodo.map((item) => (
-        <>
-          <li key={item.id} className="flex items-center mt-3">
-            {/* 토글 버튼 */}
-            <button onClick={() => toggleTodo(item.id)} className="mr-1">
-              {/* todo 토글 버튼 */}
-              {item.state === 'done' ? (
-                <span
-                  className=""
-                  onClick={() => {
-                    updateTodo(item.id, item.text);
-                  }}
-                >
-                  {/* 체크 버튼 */}
-                  <i className="bi bi-check-circle-fill"></i>
-                </span>
-              ) : (
-                <span
-                  className=""
-                  onClick={() => {
-                    updateTodo(item.id, item.text);
-                  }}
-                >
-                  {/* O 버튼 */}
-                  <i className="bi bi-circle"></i>
-                </span>
-              )}
-            </button>
-
-            {/* 각 Todo 항목에서 수정 버튼을 클릭하면, handleEditClick 함수가 호출되어 해당 항목의 isEditing 상태가 true로 변경되고, 입력 필드가 렌더링된다(편집 모드로 전환)
-              입력 필드에서 변경 사항을 저장하거나 취소하면, isEditing 상태가 null로 변경되어 다시 텍스트 모드로 전환된다 */}
-            {isEditing === item.id ? (
-              <div className="flex items-center">
-                <input
-                  type="text"
-                  value={editingText}
-                  onChange={handleInputChange}
-                  className="mr-2 rounded-md "
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                      handleSaveClick(item.id);
-                    }
-                  }}
-                />
-                <button onClick={() => handleSaveClick(item.id)}>저장</button>
-                <button onClick={handleCancelClick} className="ml-2">
-                  취소
-                </button>
-              </div>
+        <li key={item.id} className="flex items-center mt-3">
+          {/* 토글 버튼 */}
+          <button onClick={() => toggleTodo(item.id)} className="mr-1">
+            {/* todo 토글 버튼 */}
+            {item.state === 'done' ? (
+              <span
+                className=""
+                onClick={() => {
+                  updateTodo(item.id, item.text);
+                }}
+              >
+                {/* 체크 버튼 */}
+                <i className="bi bi-check-circle-fill"></i>
+              </span>
             ) : (
-              <div className="flex items-center">
-                {/* item.state가 done 이라면 취소선 */}
-                <span
-                  className={`${item.state === 'done' ? 'line-through' : ''}`}
-                >
-                  {item.text}
-                </span>
-                <button
-                  onClick={() => handleEditClick(item.id, item.text)}
-                  className="flex absolute right-14"
-                >
-                  {/* 수정 버튼 */}
-                  <i className="bi bi-pencil-fill"></i>
-                </button>
-              </div>
+              <span
+                className=""
+                onClick={() => {
+                  updateTodo(item.id, item.text);
+                }}
+              >
+                {/* O 버튼 */}
+                <i className="bi bi-circle"></i>
+              </span>
             )}
-            <span className="flex absolute right-5">
-              <button onClick={() => deleteTodo(item.id)} className="ml-3">
-                {/* 삭제 버튼 */}
-                <i className="bi bi-trash-fill"></i>
+          </button>
+
+          {/* 각 Todo 항목에서 수정 버튼을 클릭하면, handleEditClick 함수가 호출되어 해당 항목의 isEditing 상태가 true로 변경되고, 입력 필드가 렌더링된다(편집 모드로 전환)
+              입력 필드에서 변경 사항을 저장하거나 취소하면, isEditing 상태가 null로 변경되어 다시 텍스트 모드로 전환된다 */}
+          {isEditing === item.id ? (
+            <div className="flex items-center">
+              <input
+                type="text"
+                value={editingText}
+                onChange={handleInputChange}
+                className="mr-2 rounded-md "
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    handleSaveClick(item.id);
+                  }
+                }}
+              />
+              <button onClick={() => handleSaveClick(item.id)}>저장</button>
+              <button onClick={handleCancelClick} className="ml-2">
+                취소
               </button>
-            </span>
-          </li>
-        </>
+            </div>
+          ) : (
+            <div className="flex items-center">
+              {/* item.state가 done 이라면 취소선 */}
+              <span
+                className={`${item.state === 'done' ? 'line-through' : ''}`}
+              >
+                {item.text}
+              </span>
+              <button
+                onClick={() => handleEditClick(item.id, item.text)}
+                className="flex absolute right-14"
+              >
+                {/* 수정 버튼 */}
+                <i className="bi bi-pencil-fill"></i>
+              </button>
+            </div>
+          )}
+          <span className="flex absolute right-5">
+            <button onClick={() => deleteTodo(item.id)} className="ml-3">
+              {/* 삭제 버튼 */}
+              <i className="bi bi-trash-fill"></i>
+            </button>
+          </span>
+        </li>
       ))}
     </ul>
   );
