b7b887811f3488e3e69d4917b74ee52d00258570
ldw0123 <ldw0123@gmail.com>
2024-07-30 17:01:02 +0900

    🎉 next.js 프로젝트 구성 및 설계

---
+++ 06-next-app-test/features/about/components/aboutHeader/aboutHeader.tsx
@@ -0,0 +1,28 @@
+// Data Fetch
+
+/* 
+const getCustomFetch = ({path}) => {
+  fetch({baseurl}/path, {
+    method : 'GET'
+  })
+}
+const postCustomPost = ({path, body}) => {
+  fetch({baseurl}/path, {
+    method : 'POST',
+    body
+  })
+}
+
+const getCustomFetch = ({path}) => {
+  fetch({baseurl}/path, {
+    method : 'Get'
+  })
+}
+
+const userList = await getCustomFetch({'/user/list'|)
+
+apiFetcher(FETCH_USER_KEY)
+.setBody({})
+.query({})
+.fetch();
+*/
