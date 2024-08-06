8d612e82f329f753126dad225af86070e05218ed
ldw0123 <ldw0123@gmail.com>
2024-08-06 17:40:13 +0900

    ♻️ 따옴표 문자 수정

--- 06-next-app-test/app/chartblock/page.tsx
+++ 06-next-app-test/app/chartblock/page.tsx
@@ -309,7 +309,7 @@ export default function Charts() {
           <CardHeader>
             <CardTitle>Progress</CardTitle>
             <CardDescription>
-              You're average more steps a day this year than last year.
+              You&apos;re average more steps a day this year than last year.
             </CardDescription>
           </CardHeader>
           <CardContent className="grid gap-4">
@@ -698,7 +698,7 @@ export default function Charts() {
           <CardHeader className="p-4 pb-0">
             <CardTitle>Active Energy</CardTitle>
             <CardDescription>
-              You're burning an average of 754 calories per day. Good job!
+              You&apos;re burning an average of 754 calories per day. Good job!
             </CardDescription>
           </CardHeader>
           <CardContent className="flex flex-row items-baseline gap-4 p-4 pt-2">
